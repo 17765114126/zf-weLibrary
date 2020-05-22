286b282f8165db9002f06d6b8fea5002e749641b
zhaofu <17765114126@163.com>
2020-05-22 17:43:43 +0800

    getTXT部分

--- src/test/java/Test/getTXT.java
+++ src/test/java/Test/getTXT.java
@@ -19,6 +19,7 @@ public class getTXT {
     @Test
     public static void main(String[] args) {
         //共 24869 行，目前读到 2214 行
+        int currRead = 2214;
         File file = new File("E:/321.txt");
         try {
 //            BufferedReader in = new BufferedReader(new FileReader(file));
@@ -31,6 +32,9 @@ public class getTXT {
                 if (str.isEmpty()) continue;
 //                if (i > 62000 && i <= 63000) {//控制台一次打印不了全部内容，后面的会把前面的覆盖
                 while (str.length() > LINELENGTH) {//将长的行自动换行
+                    if (z == currRead){
+                        System.out.println("上次读到");
+                    }
                     System.out.println(z + "行" + i + "t" + str.substring(0, LINELENGTH));
                     str = str.substring(LINELENGTH);
                     z++;
