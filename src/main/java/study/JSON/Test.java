d2fea66b70e637168710230d43051c2a9fa584d3
zhaofu <17765114126@163.com>
2020-07-31 09:32:39 +0800

    Json

---
+++ src/main/java/study/JSON/Test.java
@@ -0,0 +1,32 @@
+package study.JSON;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.springboot.model.Person;
+
+/**
+ * @ClassName Test
+ * @Author zhaofu
+ * @Date 2020/7/31
+ * @Version V1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        Person msg = new Person();
+        msg.setAge(10);
+        msg.setName("张三");
+        msg.setSex("nan");
+//        SyjOrderMO syjOrderMO = new SyjOrderMO();
+//        syjOrderMO.setSellerBusinessId(883);
+//        msg.setSyjOrderMO(syjOrderMO);
+        //对象转Json字符串
+        String string1 = JSON.toJSONString(msg);
+
+        //Json字符串转对象
+        Person ps = JSON.parseObject(string1, Person.class);
+        Person psOne = JSONObject.parseObject(string1, Person.class);
+        System.out.println(ps);
+        System.out.println(psOne);
+    }
+
+}
