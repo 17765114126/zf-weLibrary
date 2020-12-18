022c8590e4ed81b275f902848e625fa6486c63ee
zhaofu <17765114126@163.com>
2020-12-18 14:50:48 +0800

    Shiro

--- src/main/java/com/example/springboot/config/Shiro/security/SpringUtils.java
+++ src/main/java/com/example/springboot/config/Shiro/security/SpringUtils.java
@@ -3,6 +3,7 @@ package com.example.springboot.config.Shiro.security;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 @Component
 public class SpringUtils implements ApplicationContextAware {
